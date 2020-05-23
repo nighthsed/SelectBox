3009db11f81462629ef7918dbe75802d297de5a2
nighthsed <118202822+nighthsed@users.noreply.github.com>
2020-05-23 15:22:49 +0000

    add info to readme

--- Count_Less_Than.cpp
+++ Count_Less_Than.cpp
@@ -7,3 +7,5 @@ using namespace std;
 // Pre: "v" is a vector of integers
 //  "key" is any integer
 //  "start" is a position in v
+// Post: Returns the number of elements from v[start]// to v[v.size()-1] that are < "key"
+int Count_Less_Than(const vector<int>& v, int key, int start)
