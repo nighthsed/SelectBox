fbeaed10592fd7e3b33a3adc87ee3b4a7dd84528
nighthsed <118202822+nighthsed@users.noreply.github.com>
2020-10-10 08:44:34 +0000

    merge attempt

--- My_Substr.cpp
+++ My_Substr.cpp
@@ -6,3 +6,5 @@ using namespace std;
 //Iterative solution
 // Pre: "s" is any string
 //  "start" and "end" are positions in s
+// Post: returns a string consisting of s[start]..s[end] (inclusive)
+string My_Substr(const string& s, int start, int end)
