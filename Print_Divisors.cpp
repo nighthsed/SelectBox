64a9e579b07235646de396745aae54c63cf4b8f6
nighthsed <118202822+nighthsed@users.noreply.github.com>
2021-08-31 12:41:16 +0000

    Update options

--- Print_Divisors.cpp
+++ Print_Divisors.cpp
@@ -5,4 +5,4 @@ using namespace std;
 //Iterative Solution
 // Pre: n is a positive integer
 //  "max_factor" is a positive integer <= n
-// Post: outputs to the screen all numbers <= max_fa
+// Post: outputs to the screen all numbers <= max_fact
