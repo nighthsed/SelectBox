d93f4b4b254287652dc7f5ab448953211de7111f
nighthsed <118202822+nighthsed@users.noreply.github.com>
2022-11-29 05:49:29 +0000

    config changed

--- Print_Divisors.cpp
+++ Print_Divisors.cpp
@@ -36,4 +36,4 @@ int main()
   cout << "The divisors of " << num << " <= " << factor << " are\n";
   Print_Divisors(num, factor);
   cout << "\nusing iteration and\n";
-  Recur_Print_Divisors(
+  Recur_Print_Divisors(nu
